47bd7089764e0a0eaf63a8e38be1c94afb58e423
PowerLemons <f0kent0sh@gmail.com>
2020-09-24 07:20:05 -0700

    Updates

---
+++ precache-manifest.f0515bc1521266fc327b01c471f301f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9447709616b26f6606bef3d2fdd85f57",
+    "url": "/PowerLemons/bovanti-quiz/index.html"
+  },
+  {
+    "revision": "78e73ae8df1f692266b9",
+    "url": "/PowerLemons/bovanti-quiz/static/css/main.6d8c1e35.chunk.css"
+  },
+  {
+    "revision": "797e79473eb1862c3a90",
+    "url": "/PowerLemons/bovanti-quiz/static/js/2.22df047f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PowerLemons/bovanti-quiz/static/js/2.22df047f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78e73ae8df1f692266b9",
+    "url": "/PowerLemons/bovanti-quiz/static/js/main.2db3da85.chunk.js"
+  },
+  {
+    "revision": "adda8ae8803133a4e9c0",
+    "url": "/PowerLemons/bovanti-quiz/static/js/runtime-main.41092003.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PowerLemons/bovanti-quiz/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/PowerLemons/bovanti-quiz/precache-manifest.f0515bc1521266fc327b01c471f301f8.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PowerLemons/bovanti-quiz/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
